ea171d4cb9bb5878f128c7d906c065a954fec049
jessrosenfield <rosenfield.jess@gmail.com>
2013-05-24 11:36:44 -0300

    setup layout for switches

--- Minesweeper/Game.java
+++ Minesweeper/Game.java
@@ -1,8 +1,34 @@
 public class Game {
 	public enum Level {BEGINNER, ADVANCED, EXPERT, CUSTOM};
+	private Level level;
 	
 	public Game(Level l) {
-		// TODO create switch with cases
+		
+		}
+	}
+	
+	public void levelSetter(Level l){
+		switch (l) {
+			case BEGINNER:
+				// TODO set beginner game parameters
+				break;
+			
+			case ADVANCED:
+				// TODO set advanced game parameters
+				break;
+			
+			case EXPERT:
+				// TODO set expert game parameters
+				break;
+				
+			case CUSTOM:
+				// TODO collect custom game parameters
+				// TODO set custom game parameters
+				break;
+			
+			case default:
+				// TODO setup static variable for level last played (initially beginner?)
+				// TODO get last level and impliment it
 	}
 	
 	private static class SquareSetup {
