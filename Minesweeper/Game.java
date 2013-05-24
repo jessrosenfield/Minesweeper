6e56635d148bc868f4cededbe30487a7db376ec6
jessrosenfield <rosenfield.jess@gmail.com>
2013-05-24 10:23:18 -0400

    merged github with school version

--- Minesweeper/Game.java
+++ Minesweeper/Game.java
@@ -1,14 +1,13 @@
-
 public class Game {
-	public enum level {BEGINNER, ADVANCED, EXPERT, CUSTOM};
+	public enum Level {BEGINNER, ADVANCED, EXPERT, CUSTOM};
 	
 	public Game(level l) {
-		
+		// TODO create switch with cases
 	}
 	
 	private static class SquareSetup {
 		static void main(String[] args){
-			
+			Square[][] squares;
 		}
 	}
 }
