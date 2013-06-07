40995fd30a9daaca0b9ea4123989991d8694cf87
jessrosenfield <rosenfield.jess@gmail.com>
2013-06-07 11:22:11 -0300

    Update NumberSquare.java

--- Minesweeper/NumberSquare.java
+++ Minesweeper/NumberSquare.java
@@ -1,39 +1,12 @@
 public class NumberSquare implements Square {
-	private boolean revealed;
-	private int row;
-	private int column;
 	private int squareNum;
 	
-	public NumberSquare(int sqNum, int r, int c){
-		revealed = false;
-		row = r;
-		column = c;
+	public NumberSquare(int r, int c, int sqNum){
+		super(r, c);
 		squareNum = sqNum;
 	}
-	
+	@Override
 	public int getNum(){
 		return squareNum;
-	}
-	
-	@Override
-	public int reveal() {
-		revealed = true;
-		return squareNum;
-	}
-	@Override
-	public boolean isRevealed() {
-		return revealed;
-	}
-	@Override
-	public int getRow() {
-		return row;
-	}
-	@Override
-	public int getColumn() {
-		return column;
-	}
-	@Override
-	public int[] getRC() {
-		return {row, column};
 	}
 }
