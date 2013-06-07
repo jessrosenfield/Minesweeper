7e03bc6f32e12d46f6157ff99e5cd142a871ddfd
jessrosenfield <rosenfield.jess@gmail.com>
2013-06-07 11:06:17 -0300

    Square is abstract class

--- Minesweeper/Square.java
+++ Minesweeper/Square.java
@@ -1,8 +1,29 @@
-public interface Square {
-	public int getRow();
-	public int getColumn();
-	public int[] getRC();
-	public boolean isRevealed();
-	public int reveal();
-	public int getNum();
+public abstract class Square {
+    private boolean revealed;
+    private int row;
+    private int column;
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public int[] getRC() {
+        int[] RC = {row, column};
+        return RC;
+    }
+
+    public boolean isRevealed() {
+        return revealed;
+    }
+
+    public int reveal() {
+        revealed = true;
+        return getNum();
+    }
+
+    public abstract int getNum();
 }
