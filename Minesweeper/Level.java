5fe3c8da8711a24b24b9e53712310920404cf854
jessrosenfield <rosenfield.jess@gmail.com>
2013-06-07 11:18:49 -0300

    Update Level.java

--- Minesweeper/Level.java
+++ Minesweeper/Level.java
@@ -1,3 +1,18 @@
 public class Level {
+  private int rows;
+  private int columns;
+  private int mines;
   
+  public int getRows() {
+    return rows;
+  }
+  public int getColumns() {
+    return columns;
+  }
+  public int getMines() {
+    return mines;
+  }
+  public int[] getRC() {
+    return {rows, columns};
+  }
 }
