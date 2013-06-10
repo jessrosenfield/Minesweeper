cf462715b6132740aea638e268269f5517a7646b
jessrosenfield <rosenfield.jess@gmail.com>
2013-06-10 13:31:32 -0400

    Update GUI.java

--- Minesweeper/GUI.java
+++ Minesweeper/GUI.java
@@ -1,26 +1,3 @@
- import java.awt.*; 
- import javax.swing.*; 
- public class GUI
- { 
-    private static void createWindow() {
-       JFrame frame = new JFrame("Minesweeper");
-       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-       JLabel textLabel = new JLabel("Minesweeper", SwingConstants.CENTER); 
-       textLabel.setPreferredSize(new Dimension(500, 500)); 
-       frame.getContentPane().add(textLabel, BorderLayout.NORTH); 
-       frame.setLocationRelativeTo(null); 
-       frame.pack();
-       frame.setVisible(true);
-    }
-    public static void main(String[] args) {
-       createWindow();
-    }
- } 
-
-
-
-
-
 import java.awt.*;
 import javax.swing.*; 
 public class GUI
@@ -28,21 +5,30 @@ public class GUI
     private static JFrame createWindow()
     {
         JFrame frame = new JFrame("Minesweeper");
+        //noframegrid bpanel = new noframegrid(9, 9);
+        noframegrid bpanel = new noframegrid(9, 9);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-        JLabel textLabel = new JLabel("Minesweeper", SwingConstants.CENTER); 
-        textLabel.setPreferredSize(new Dimension(600, 500)); 
-        frame.getContentPane().add(textLabel, BorderLayout.NORTH);
+        
+        
+        
+        frame.setPreferredSize(new Dimension(500, 500));
+       
         frame.setLocationRelativeTo(null); 
+       frame.add(bpanel);
+       frame.setResizable(false);
+         frame.pack();
         frame.setVisible(true);
-        frame.pack();
-        new ButtonGrid(9,9);
+        
+       
         return frame;
     }
     public static void main(String[] args)
     {
-       new ButtonGrid(9,9);
+       //new noframegrid(9,9);
+       createWindow();
     }
 } 
+
 
 
 
