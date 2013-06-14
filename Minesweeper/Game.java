ae181d057d39ce00c319fe0ba99f388a38aec817
jessrosenfield <rosenfield.jess@gmail.com>
2013-06-14 12:13:29 -0300

    Update Game.java

--- Minesweeper/Game.java
+++ Minesweeper/Game.java
@@ -1,4 +1,5 @@
 public class Game {
+    
     public static enum Level {
         BEGINNER    (9, 9, 10), 
         ADVANCED    (16, 16, 40), 
@@ -13,19 +14,77 @@ public class Game {
             mines = m;
         }
     }
+    
     public final Level level;
-    private static Level lastLevel = Level.BEGINNER;
-    public int flags;
-    private Square[][] squares;
+    
+    private static Level lastLevel; //default constructor uses the same level as before.
+    lastLevel = Level.BEGINNER; //sets lastLevel to beginner in case there was no previous game
+    
+    public int flags;   //to be used with counter of mines left
+    
+    private Square[][] squares; //the grid of squares
 
+/*******************************************************************************************/
     public Game(Level l) {
         level = l;
         lastLevel = l;
         squares = new Square[l.rows][l.columns]
+        placeSquares();
     }
     
     public Game() {
-        level = lastLevel;
-        squares = new Square[lastLevel.rows][lastLevel.columns]
+        Game(lastLevel);
     }
+    
+    
+/********************************************************************************************/    
+        
+    public void placeSquares() {
+        for(int r = 0; r < level.rows; r++)
+            for(int c = 0; c < level.columns; c++)
+                Square[r][c] = new emptySquare(r, c);
+    }
+    
+    private boolean badNums(Square[] sq, int r, int c) {
+        for(Square s : sq) {
+            if(s.getRow == r && s.getColumn == c)
+                return true;
+        }
+        return false;
+    }
+    
+    private Squares[] surroundings() {
+        
+    }
+    
+    public void levelSetup(int row, int col) {
+        Square[] offLim = new Square[mines+6]
+        int counter = 0;
+        offLim[0] = Squares[row][col];
+        counter++;
+        
+        for(int m = 0; m < level.mines; m++) {
+            int i; int j;
+            
+            do {
+                i = Math.random()*rows;
+                j = Math.random()*columns
+            } while( badNums(offLim, i, j) );
+            
+            Squares[i][j] = new mineSquare(i, j);
+            offLim[counter] = Squares[i][j];
+            counter++;
+                )
+        }
+        
+    }
+    
+    public void levelSetup(Square firstSquare) {
+        Square fs = firstSquare;
+        int r = fs.getRow();
+        int c = fs.getColumn;
+        levelSetup(r, c);
+    }
+    
+    
 }
