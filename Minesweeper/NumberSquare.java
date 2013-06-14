431bcd71d60ca2b11f3d3cbf848382bba85a7813
jessrosenfield <rosenfield.jess@gmail.com>
2013-06-14 11:48:29 -0300

    Update NumberSquare.java

--- Minesweeper/NumberSquare.java
+++ Minesweeper/NumberSquare.java
@@ -1,12 +1,17 @@
 public class NumberSquare implements Square {
 	private int squareNum;
 	
-	public NumberSquare(int r, int c, int sqNum){
+	public NumberSquare(int r, int c, int sqNum) {
 		super(r, c);
 		squareNum = sqNum;
 	}
+	
 	@Override
-	public int getNum(){
+	public int getNum() {
 		return squareNum;
+	}
+	
+	public void addNum() {
+		squareNum++;
 	}
 }
