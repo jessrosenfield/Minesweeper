862665001e36571f10b429666cbab8635fca6b73
Jessica Rosenfield <rosenfield.jess@gmail.com>
2013-06-15 16:11:31 -0400

    commit silly changes

--- Minesweeper/GUI.java
+++ Minesweeper/GUI.java
@@ -6,18 +6,18 @@ public class GUI
     private static JFrame createWindow()
     {
         JFrame frame = new JFrame("Minesweeper");
-        noframegrid bpanel = new noframegrid(9, 9);
+        noframegrid bpanel = new noframegrid(16, 16);
         JButton topbtn = new JButton();
-        topbtn.setPreferredSize(new Dimension(500, 50));
+        topbtn.setPreferredSize(new Dimension(50, 50));
         
         frame.add(topbtn, BorderLayout.NORTH);
         
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
-        frame.setPreferredSize(new Dimension(500, 550));
+        frame.setPreferredSize(new Dimension(50*16, 55*16));
         
         frame.setLocationRelativeTo(null); 
         frame.add(bpanel);
-        frame.setResizable(false);
+        frame.setResizable(true);
         frame.pack();
         frame.setVisible(true);
         return frame;
