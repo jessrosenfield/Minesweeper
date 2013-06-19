56ed89c1df4e76eff0781e7a0ad34141db999f5f
Jessica Rosenfield <rosenfield.jess@gmail.com>
2013-06-19 19:27:19 -0400

    nocluewhatimdoing

--- Minesweeper/graphics/GUI.java
+++ Minesweeper/graphics/GUI.java
@@ -1,61 +1,26 @@
 package graphics;
-/*import java.awt.*;
-import javax.swing.*; 
-public class GUI
 
-{
-    private static JFrame createWindow()
-    {
-        JFrame frame = new JFrame("Minesweeper");
-        Game game = new Game();
-        noframegrid bpanel = new noframegrid(game.level.rows, game.level.columns);
-        JButton topbtn = new JButton();
-        topbtn.setPreferredSize(new Dimension(50, 50));
-        
-        frame.add(topbtn, BorderLayout.NORTH);
-        
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
-        frame.setPreferredSize(new Dimension(50*16, 55*16));
-        
-        frame.setLocationRelativeTo(null); 
-        frame.add(bpanel);
-        frame.setResizable(true);
-        frame.pack();
-        frame.setVisible(true);
-        return frame;
-    }
-    public static void main(String[] args)
-    {
-       createWindow();
-    }
-} 
-
-
-
-
-
-*/
+import javax.swing.*;
 
 import game.Game;
 
 import java.awt.*;
 import javax.swing.*; 
 
+@SuppressWarnings("unused")
 public class GUI
 {
    private static JFrame createWindow()
    {
        JFrame frame = new JFrame("Minesweeper");
-       ControlPanel cpanel = new ControlPanel();
        Game game = new Game();
        int rows = game.level.rows;
        int cols = game.level.columns;
-       noframegrid bpanel = new noframegrid(rows, cols);
+       noframegrid bpanel = new noframegrid(game, rows, cols);
        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
        frame.setPreferredSize(new Dimension(26*rows, 26*cols+100));       
        frame.setLocationRelativeTo(null); 
        frame.add(bpanel, BorderLayout.NORTH);
-       frame.add(cpanel, BorderLayout.NORTH);
        frame.setResizable(false);
        frame.pack();
        frame.setVisible(true);
--- Minesweeper/graphics/noframegrid.java
+++ Minesweeper/graphics/noframegrid.java
@@ -1,77 +1,51 @@
 package graphics;
-import javax.imageio.*;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.*;
 import javax.swing.*;
-import javax.swing.border.*;
-import java.io.IOException;
 
-public class noframegrid extends JPanel implements ActionListener
-{
+import game.*;
+
+@SuppressWarnings("serial")
+public class noframegrid extends JPanel implements ActionListener {
 	static JPanel panel = new JPanel();
-	JButton[][] grid;
+	Button[][] bGrid;
+	JButton[][] jGrid;
 	int width;
 	int length;
-	public noframegrid(int w, int l)
-	{
+	Game game;
+	
+	public noframegrid(Game g, int w, int l) {
 		width = w;
 		length = l;
+		game = g;
 		setLayout(new GridLayout(width,length));
-		grid = new JButton[width][length];
-		//panel.setResizable(false);
-		for(int y = 0; y < length; y++)
-		{
-			for(int x = 0; x < width; x++)
-			{
-				grid[x][y] = new JButton("");   
-				add(grid[x][y]);
+		bGrid = new Button[width-1][length-1];
+		jGrid = new JButton[width-1][length-1];
+		for(int y = 0; y < length; y++) {
+			for(int x = 0; x < width; x++) {
+				try {
+					bGrid[x][y] = new Button(game.getsq(x, y));
+					jGrid[x][y] = bGrid[x][y].JB;
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 		}
-
-		//panel.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		grid[width-1][length-1].addActionListener(this);
-		Image img;
-		ImageIcon imgi=null;
-		//panel.pack(); 
+		
 		setVisible(true);
 		setSize(new Dimension(25*width,25*length));
-		try {
-			img = ImageIO.read(getClass().getResource("Stone.png"));
-			imgi = new ImageIcon(img);
-		}catch (IOException e){}
-		for(int r = 0; r < length; r++)
-		{
-			for(int c = 0; c < width; c++)
-			{
-				JButton b = grid[r][c]; 
-				if(imgi!=null)
-					b.setIcon(imgi);
-			}
-		}
 	}
 
-	public void actionPerformed(ActionEvent e)
-	{
-		if ("click0,0".equals(e.getActionCommand()))
-		{
-			Image img;
-			ImageIcon imgi=null;
-			//frame.pack(); 
-			try {
-				img = ImageIO.read(getClass().getResource("Stone2.png"));
-				imgi = new ImageIcon(img);
-			}catch (IOException c){}
-		}
-	}
-
-	public static JPanel returnPanel()
-	{
+	public static JPanel returnPanel() {
 		return panel;
 	}
 
-	public static void main(String[] args)
-	{
-		new noframegrid(9, 9);
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 }
